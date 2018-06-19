97189b0764730cb1c06156c6fba1ea01925463ac
Nelson Sandoval <techednelson@gmail.com>
2018-06-19 17:46:43 +0300

    feat: Implement dao package

--- src/main/java/web/PersonsServlet.java
+++ src/main/java/web/PersonsServlet.java
@@ -12,7 +12,7 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.List;
 
-@WebServlet("/personas")
+@WebServlet("/persons")
 public class PersonsServlet extends HttpServlet {
 
     @EJB
